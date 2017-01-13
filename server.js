9d437ed98f26efb119b807c9235aa37688a2610b
Samet Oussema <sametoussema@gmail.com>
2017-01-13 23:11:49 +0100

    testing continious deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT=8001;
 const express=require('express');
 const app=express();
 app.get('/',function(req,res){
-	res.status(200).send('server is running');
+	res.status(200).send('server is running! dedicase For Abbes !!');
 }).use(function(req,res,next){
     res.status(404).send('Page introuvable !');
 }).listen(PORT);
