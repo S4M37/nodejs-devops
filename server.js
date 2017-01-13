85b7faf68ec0d29ff1775d06e16c313da3d14d5b
Samet Oussema <sametoussema@gmail.com>
2017-01-13 22:22:10 +0100

    fixing server.js

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-const PORT=8080;
+const PORT=8001;
 const express=require('express');
 const app=express();
-app.get('/welcome',function(req,res){
-	res.status(200).render('welcome.ejs');
-})
-.get('/call-my-module',function(req,res){
-	var myModule=require('my_module');
-	myModule.sayHello();
-	myModule.saySalem();
-		res.status(200).send('module called successfully !');
-
+app.get('/',function(req,res){
+	res.status(200).send('server is running');
 }).use(function(req,res,next){
     res.status(404).send('Page introuvable !');
 }).listen(PORT);
