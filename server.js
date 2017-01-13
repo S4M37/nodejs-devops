8ab09430a19be36010ff0dabf6bdacc587b87400
Samet Oussema <sametoussema@gmail.com>
2017-01-13 22:30:25 +0100

    fixing host url in server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get('/',function(req,res){
 }).use(function(req,res,next){
     res.status(404).send('Page introuvable !');
 }).listen(PORT);
-console.log('Running on http://localhost:' +PORT);
+console.log('Running on http://104.46.32.90:' +PORT);
